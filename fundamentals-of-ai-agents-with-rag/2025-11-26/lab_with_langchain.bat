1240b0ca4569dc82b17155eabdd18d818bf0fbdd
Jim <jimkoch@gmail.com>
2025-11-28 18:08:43 -0500

    2025-11-26: Fundamentals of AI Agents Using RAG: LangChain Documents.

---
+++ fundamentals-of-ai-agents-with-rag/2025-11-26/lab_with_langchain.bat
@@ -0,0 +1,10 @@
+pip install --force-reinstall --no-cache-dir tenacity --user
+pip install "ibm-watsonx-ai==1.0.4" --user
+pip install "ibm-watson-machine-learning==1.0.357" --user
+pip install "langchain-ibm==0.1.7" --user
+pip install "langchain-community==0.2.1" --user
+pip install "langchain-experimental==0.0.59" --user
+pip install "langchainhub==0.1.17" --user
+pip install "langchain==0.2.1" --user
+pip install "pypdf==4.2.0" --user
+pip install "chromadb == 0.4.24" --user
